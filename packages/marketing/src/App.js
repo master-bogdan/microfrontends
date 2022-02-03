03b4f15c20902f07500d9231de73f22f20643f7a
Bogdan <maiskiychavk@gmail.com>
2022-02-03 19:56:19 +0200

    [add navigation]

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  Router,
+} from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 // Components
 import Landing from './components/Landing';
@@ -9,18 +13,18 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma',
 });
 
-const App = () => {
+const App = ({ history }) => {
   return (
-    <div>
+    <>
       <StylesProvider generateClassName={generateClassName}>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
-    </div>
+    </>
   )
 }
 
