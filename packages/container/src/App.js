4bb0da112e7cce1191bf99914682abf18c45d0af
Bogdan <maiskiychavk@gmail.com>
2022-02-03 12:57:39 +0200

    [add random css prefixes]

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+// Components
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
